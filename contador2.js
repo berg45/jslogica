dae41a20ad2e5d08a92d3c4e80c7dbc345f020c2
berg45 <pereiraberg62@gmail.com>
2023-11-06 15:16:29 -0300

    jsLogica.js

---
+++ contador2.js
@@ -0,0 +1,6 @@
+var cont = 0
+
+do {
+    cont++ // cont = cont = 1
+    console.log(cont)
+} while (cont < 10);
---
+++ dividir_valores.js
@@ -0,0 +1,22 @@
+const prompt = require('prompt-sync')()
+
+var n1, n2, resultado, continua, opcao
+
+continua = true
+
+while (continua) {
+   var n1 = parseFloat(prompt('digite o primeiro valor: '))
+   var n2 = parseFloat(prompt('digite o segundo valor: '))
+
+   if (n2 === 0) {
+       console.log('número invalido')
+       var n2 = parseFloat(prompt('digite o segundo valor'))
+   }
+
+   resultado = n1 / n2
+   console.log(`O resultado é ${resultado}`)
+   opcao = prompt('Deseja continuar com a operação? S - Sim | N - Não')
+   if (opcao.toUpperCase() === 'N') {
+       continua = false
+   } 
+}
